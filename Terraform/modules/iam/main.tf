1b7867793d4fc0c9831a235b78db9d2979733cb4
Abdul Salim <100780536+salim1704@users.noreply.github.com>
2026-02-01 09:14:17 +0000

    iam roles for ecs

---
+++ Terraform/modules/iam/main.tf
@@ -0,0 +1,22 @@
+resource "aws_iam_role" "this" {
+    name = "ecsTaskExecutionRole"
+    assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowAccessToECSForTaskExecutionRole",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "ecs-tasks.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+)
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_exec_policy" {
+  role       = aws_iam_role.this.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
